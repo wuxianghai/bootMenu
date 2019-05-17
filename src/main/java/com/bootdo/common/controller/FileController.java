e6d64649bd0e029037c2ca674c7e21b9e94796d3
wuxianghai <wu_xianghai@163.com>
2019-05-17 16:20:56 +0800

    11

--- src/main/java/com/bootdo/common/controller/FileController.java
+++ src/main/java/com/bootdo/common/controller/FileController.java
@@ -140,9 +140,6 @@ public class FileController extends BaseController {
 	@ResponseBody
 	@PostMapping("/upload")
 	R upload(@RequestParam("file") MultipartFile file, HttpServletRequest request) {
-//		if ("test".equals(getUsername())) {
-//			return R.error(1, "演示系统不允许修改,完整体验请部署程序");
-//		}
 		String fileName = file.getOriginalFilename();
 		fileName = FileUtil.renameToUUID(fileName);
 		FileDO sysFile = new FileDO(FileType.fileType(fileName), "/files/" + fileName, new Date());
@@ -158,5 +155,22 @@ public class FileController extends BaseController {
 		return R.error();
 	}
 
+	@ResponseBody
+	@PostMapping("/uploadFriend")
+	R uploadFriend(@RequestParam("file") MultipartFile file, HttpServletRequest request) {
+		String fileName = file.getOriginalFilename();
+		fileName = FileUtil.renameToUUID(fileName);
+		FileDO sysFile = new FileDO(99, "/files/" + fileName, new Date());
+		try {
+			FileUtil.uploadFile(file.getBytes(), bootdoConfig.getUploadPath(), fileName);
+		} catch (Exception e) {
+			return R.error();
+		}
+
+		if (sysFileService.save(sysFile) > 0) {
+			return R.ok().put("fileName",sysFile.getUrl());
+		}
+		return R.error();
+	}
 
 }
